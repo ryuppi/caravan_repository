79d489ca9efb80f013704f6e782565b6c680cc83
ryuppi <rys612x@gmail.com>
2018-02-27 16:33:40 +0000

    second commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,33 @@
 class BlogsController < ApplicationController
 	def index
+		@blog = Blog.all
+	end
+
+	def show
+		@blog = Blog.find(params[:id])
+		#findメソッドはidを使ってモデルからレコードを探す
+	end
+
+	def new
+		@blog = Blog.new
+		#ブログモデルと紐付いた（データを入れるための）からの箱を定義
+		#インスタンス変数
+	end
+
+	def create
+		#ストロングパラメータを使用 rails３以降？は直接入れるとエラーが出る
+		blog = Blog.new(blog_params)
+		#データベースへ保存
+		blog.save
+		#新規投稿画面へリダイレクト
+		#redirect_to "/blogs"
+		redirect_to blog_path(blog.id)
+	end
+
+	private
+
+	def blog_params
+		params.require(:blog).permit(:title, :body, :category)
+		#Viewからそうしんされたデータはparamsというメソッドの中に入っている
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/blogs/new'
+  #blogs/newをblogs/:idの下に書くと、newがidと勘違いされるため、上に書かないとダメ！！
+  post '/blogs' => 'blogs#create'
   get '/blogs' => 'blogs#index'
+  get '/blogs/:id' => 'blogs#show', as: 'blog'
 end
